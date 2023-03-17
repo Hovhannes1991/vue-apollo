d6231d77c63bea380f9c7cddd0101496ffce0c59
Hovhannes1991 <hovhanneskeshishyan@yahoo.com>
2023-03-17 15:44:09 +0400

    fix little bug

--- BACK-END/server/app.js
+++ BACK-END/server/app.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 app.use(cors());
 
